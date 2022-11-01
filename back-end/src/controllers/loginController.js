b8a5a94662210837312d44d4117a6ea83f2ef3c7
Anthony Cruz <tony_esqueloto@hotmail.com>
2022-11-01 16:58:07 -0300

    fix: adiciona importaçoes do "dotenv" nos arquivos decriacao e validacao de token que estavam faltando

--- back-end/src/controllers/loginController.js
+++ back-end/src/controllers/loginController.js
@@ -3,7 +3,6 @@ const loginService = require('../services/loginService');
 const errorMessage = { status: 404, message: 'Not found' };
 
 const loginController = async (req, res) => {
-  // console.log("userData");
   const { email, password } = req.body;
   if (!email || !password) throw errorMessage;
   const token = await loginService.login(email, password);
