d427dc2aff274483076aed72333871983c8b7ee8
eliel <silvaeliel940@gmail.com>
2022-11-01 23:58:26 -0300

    error: login route

--- back-end/src/helpers/generateToken.js
+++ back-end/src/helpers/generateToken.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
+const fs = require('fs');
+const FILE_SECRET_JWT = "../../jwt.evaluation.key";
 
 const jwtConfig = {
   expiresIn: '5d',
@@ -7,9 +9,19 @@ const jwtConfig = {
 };
 
 const jwtKey = require("fs")
-  .readFileSync("../../jwt.evaluation.key", { encoding: "utf-8" });
+  .readFileSync("./jwt.evaluation.key", { encoding: "utf-8" });
+// async function getSecretJwt() {
+//   const jwtKeyGet = await fs.readFileSync(FILE_SECRET_JWT, 'utf-8')
+//   // .catch((err) => {
+//   //   console.log(err);
+//   //   return false;
+//   // });
+//   return jwtKeyGet;
+// }
 
-module.exports = (body) => {
+module.exports = async (body) => {
+  // const jwtKey = fs.readFileSync('jwt.evaluation.key.',"utf-8" )
+  console.log( jwtKey )
   const token = jwt.sign(
     body,
     jwtKey,
