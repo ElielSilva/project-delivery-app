977be1f9f78550ae17955a0c06bc9c1779c529e3
ThiagoDC1001 <cmsthiagoazevedo@gmail.com>
2022-11-04 15:19:26 -0300

    fix: lint

--- back-end/src/helpers/generateToken.js
+++ back-end/src/helpers/generateToken.js
@@ -1,16 +1,13 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
-const fs = require('fs');
-const FILE_SECRET_JWT = "../../jwt.evaluation.key";
 
 const jwtConfig = {
   expiresIn: '5d',
   algorithm: 'HS256',
 };
 
-const jwtKey = require("fs")
-  .readFileSync("./jwt.evaluation.key", { encoding: "utf-8" });
-
+const jwtKey = require('fs')
+  .readFileSync('./jwt.evaluation.key', { encoding: 'utf-8' });
 
 module.exports = async (body) => {
   const token = jwt.sign(
--- back-end/src/routes/customerRouter.js
+++ back-end/src/routes/customerRouter.js
@@ -2,11 +2,9 @@ const { Router } = require('express');
 const checkoutController = require('../controllers/checkoutController');
 const costumerProductsController = require('../controllers/costumerProductsController');
 
-
 const customerRouter = Router();
 
 customerRouter.post('/orders', checkoutController.create);
-customerRouter.get('/products', costumerProductsController)
-
+customerRouter.get('/products', costumerProductsController);
 
 module.exports = customerRouter;
