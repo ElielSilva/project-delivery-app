0d238a0095c5f78b73f6ff75cf1bab7e73182afa
Anthony Cruz <tony_esqueloto@hotmail.com>
2022-11-04 18:29:23 -0300

    fix: arruma alguns erros de lint

--- back-end/src/services/salesService.js
+++ back-end/src/services/salesService.js
@@ -1,32 +1,12 @@
 const { SalesProducts, Sales } = require('../database/models');
 
-// const exampleObj = {
-//   userId: 1,
-//   sellerId: 1,
-//   totalPrice: 100.00,
-//   deliveryAddress: 'street',
-//   deliveryNumber: 1,
-//   status: 'pending',
-//     sales: [{ productId: 1, quantity: 1 }],
-// }
-
 module.exports = {
   create: async (saleData) => {
-    const { userId,
-      sellerId,
-      totalPrice,
-      deliveryAddress,
-      deliveryNumber,
-      sales
-    } = saleData;
+    const { userId, sellerId, totalPrice, deliveryAddress,
+      deliveryNumber, sales } = saleData;
 
     const { dataValues } = await Sales.create({
-      userId,
-      sellerId,
-      totalPrice,
-      deliveryAddress,
-      deliveryNumber,
-    });
+      userId, sellerId, totalPrice, deliveryAddress, deliveryNumber });
 
     await sales.forEach(({ productId, quantity }) => {
       SalesProducts.create({
