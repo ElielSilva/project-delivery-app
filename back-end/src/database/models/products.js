69d5818fff239ebe32c8e6e547625ce6a49cc35f
eliel <silvaeliel940@gmail.com>
2022-11-09 19:09:35 -0300

    fix: fixed datatestIds

--- back-end/src/database/models/products.js
+++ back-end/src/database/models/products.js
@@ -2,7 +2,7 @@ module.exports = (sequelize, DataTypes) => {
   const Products = sequelize.define('Products', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     name: DataTypes.STRING(100),
-    price: DataTypes.DECIMAL(4,2),
+    price: DataTypes.FLOAT(4,2),
     url_image: DataTypes.STRING(200),
   },
   {
